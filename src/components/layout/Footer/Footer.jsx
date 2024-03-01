c8240c1cfcce4dea223810f16bbeea7aae28a89b
Facundo Pettersson <petterssonfacundo@gmail.com>
2024-03-01 17:58:31 -0300

    hey

---
+++ src/components/layout/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Footer = () => {
+  return (
+    <div>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum itaque beatae unde perferendis delectus voluptatibus, perspiciatis eaque quos maxime quis at est aut magnam neque placeat saepe ea, iste dolores.</p>
+    
+    </div>
+  )
+}
